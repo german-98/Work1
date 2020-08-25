985cd71ac6fc662979e9e0871a6d0ae4caf9abf3
Zheleznyj German <you@example.com>
2020-08-25 15:45:56 +0600

    #2Часть работы

--- js/slide.js
+++ js/slide.js
@@ -1,16 +1,17 @@
 const slider = document.querySelector('.swiper-container') ;
 
 var mySwiper = new Swiper(slider, {
-slidesPerView: 3,
+slidesPerView: 2,
 spaceBetween:10,
 pagination: {
     el: '.swiper-pagination',
-    type: 'fraction',
+    type: 'bullets',
     clickabale:true,
   },
   slideToClickedSlide:true,
   centeredSlides:true,
   loop:true,
+  lazy:true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
