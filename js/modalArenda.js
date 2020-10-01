4f594c22f6980bc9941c6dc3bcb58e146698e3f3
Zheleznyj German <you@example.com>
2020-10-01 14:03:42 +0600

    #9Модальное окно аренда выбор гг мм дд

--- js/modalArenda.js
+++ js/modalArenda.js
@@ -9,8 +9,43 @@ spanArenda.onclick = function () {
     arenda.style.display = "none"
 }
 
-window.onclick = function(event){
-    if(event.target == arenda ){
+window.onclick = function (event) {
+    if (event.target == arenda) {
         arenda.style.display = "none"
     }
 }
+
+$('input').each(function(){
+    var counter = 0;
+    var $this = $(this);
+    $(this).prev().on('click', function(){
+        if($this.attr('name') == 'year' && counter <2020){
+            counter = 2020;
+        }
+        counter++;
+        if($this.attr('name') == 'day' && counter > 31){
+            counter = 31;
+        }else if($this.attr('name') == 'year' && counter > 2200){
+            counter = 2200;
+        }
+        if ($this.attr('name') == 'month' && counter > 12){
+            counter = 12;
+        }
+        $this.val(counter)
+    });
+    $(this).next().on('click',function(){
+        counter--;
+        if($this.attr('name') == 'day' && counter < 1){
+            counter = 1;
+
+        }else if($this.attr('name') == 'year' && counter < 2020){
+            counter = 2020;
+        }
+        if($this.attr('name') == 'month' && counter < 1){
+            counter = 1;
+
+        }
+        $this.val(counter)
+    })
+})
+
